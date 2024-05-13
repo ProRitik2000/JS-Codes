696a183ec0674072f878105ef531bdba9cfb15e8
Ritik Pal <palritik315@gmail.com>
2024-05-13 10:51:48 +0530

    Conversion

---
+++ practice.js
@@ -0,0 +1,11 @@
+// let score="878gh";
+let isLoggedIn=[]
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let valueInNumber=Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+// console.log(typeof isLoggedIn);
+let valueInNumber=Boolean(isLoggedIn)
+console.log(valueInNumber);
